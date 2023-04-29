27f16bdf9dff71d823af89c3ead5b078a050dd65
TunanshV <123402150+TunanshV@users.noreply.github.com>
2023-04-29 11:41:05 +0530

    Add files via upload
    Just a simple c program to demonstrate use of functions

---
+++ 11 december.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void printHello();
+int main()
+{
+    printHello();//function call
+    printHello();
+    printHello();
+    printHello();
+    printHello();
+    return 0;
+}
+ //function declaration
+ void printHello(){
+ printf("Hello \n");
+}
+
